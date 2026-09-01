e638c41a322fc0e3d492f65584a3b2e4b78cebd5
Bayram Barış Gök <138613519+Bbar1sgok@users.noreply.github.com>
2026-09-01 19:19:24 +0300

    Update prime-number-checker.cpp

--- prime-number-checker.cpp
+++ prime-number-checker.cpp
@@ -5,15 +5,15 @@ using namespace std;
 
 int main()
 {  
-    int inputNumber;
+    int input_number;
     
     cout << "Please enter the number you want to check for primality: \n\n";
     
     // Input validation loop (filters out values less than or equal to 1)
     while (true) {
-        cin >> inputNumber;
+        cin >> input_number;
         
-        if (cin.fail() || inputNumber <= 1) {
+        if (cin.fail() || input_number <= 1) {
             cout << "Invalid input! Please enter an integer greater than 1: \n";
             cin.clear(); 
             cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
@@ -25,18 +25,18 @@ int main()
     
     bool isPrime = true;
 
-    // Optimization: Loop runs up to the square root of the number (i * i <= inputNumber)
-    for (int i = 2; i * i <= inputNumber; i++) {
-        if (inputNumber % i == 0) {
+    // Optimization: Loop runs up to the square root of the number (i * i <= input_number)
+    for (int i = 2; i * i <= input_number; i++) {
+        if (input_number % i == 0) {
             isPrime = false;
             break; // Standard practice: exit the loop immediately if a divisor is found
         }
     }
     
     if (isPrime) {
-        cout << inputNumber << " is a prime number.\n";
+        cout << input_number << " is a prime number.\n";
     } else {
-        cout << inputNumber << " is not a prime number.\n";
+        cout << input_number << " is not a prime number.\n";
     }
 
     return 0;
